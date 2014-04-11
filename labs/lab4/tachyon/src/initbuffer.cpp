31edbc0c917a7341f494d3be4b723496473b5d81
Wes Erickson <inflamedspirit@gmail.com>
2014-04-11 10:54:27 -0700

    Fixed Lab 4.

--- labs/lab4/tachyon/src/initbuffer.cpp
+++ labs/lab4/tachyon/src/initbuffer.cpp
@@ -108,9 +108,9 @@ int iteration_count(0);
         // First (slower) method of filling array
         // Array is NOT filled in consecutive memory address order
         /**********************************/
-        for (int i = 0; i < mem_array_i_max; i++)
-        {
-/************************/
+  //  for (int i = 0; i < mem_array_i_max; i++)
+  //    {
+/************************
 // Try to defeat hardware prefetching by varying the stride
 int j(0), iteration_count(0);
 
@@ -141,7 +141,7 @@ int iteration_count(0);
         // Faster method of filling array
         // The for loops are interchanged
         // Array IS filled in consecutive memory address order
-        /**********************************
+        /**********************************/
         for (int j = 0; j < mem_array_j_max; j++)
         {
                 for (int i = 0; i < mem_array_i_max; i++)
@@ -150,5 +150,6 @@ int iteration_count(0);
                 }
         }
         /***********************************/
-}
+	//}
 
+}
